ad6cc01ee45e5bd2b1d80c3bc31cb7ef8fad2542
Matteo Baiguini <baiguini.matteo@gmail.com>
2018-06-14 14:56:40 +0200

    Spring Tracing prototypes

---
+++ spring-opentracing-sample/src/main/java/com/rabbitshop/springopentracingsample/SpringOpentracingSampleApplication.java
@@ -0,0 +1,14 @@
+package com.rabbitshop.springopentracingsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringOpentracingSampleApplication {
+
+	public static void main(final String[] args) {
+
+		SpringApplication.run(SpringOpentracingSampleApplication.class, args);
+	}
+}
---
+++ spring-opentracing-sample/src/main/java/com/rabbitshop/springopentracingsample/configs/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.rabbitshop.springopentracingsample.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@Order(100)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/*
+	 * PLEASE NOTE: For sake of simplicity we are omitting specific security configurations
+	 */
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+
+		log.warn("Loading INSECURE security config...");
+
+		http
+				.authorizeRequests()
+				.anyRequest().permitAll()
+
+				.and()
+				.httpBasic()
+
+				.and()
+				.cors().disable()
+				.csrf().disable();
+	}
+
+}
